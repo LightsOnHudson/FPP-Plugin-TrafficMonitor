4601974d26472835e83b4d162e902a7be57b1f1b
LightsOnHudson <bshaver@MBAIR-TDL.home.lan>
2017-12-06 21:32:09 -0700

    Update

--- plugin_setup.php
+++ plugin_setup.php
@@ -95,7 +95,7 @@ switch($CAPTURE_CMD) {
 				$PID_TO_KILL = $PID_PARTS[0];
 				
 				//cmd to kill
-				$CMD_TO_KILL = "/usr/bin/sudo /usr/bin/kill -9 ".$PID_TO_KILL;
+				$CMD_TO_KILL = "/usr/bin/sudo kill -9 ".$PID_TO_KILL;
 				
 				
 				if($DEBUG) {
