a29e5774701c0304adf5dd2a418b02aa87965f86
LightsOnHudson <bshaver@MBAIR-TDL.home.lan>
2017-12-18 18:50:21 -0700

    Update

--- today.php
+++ today.php
@@ -7,8 +7,9 @@ $END_DATE = $_GET['END_DATE'];
 $END_HOUR = $_GET['END_HOURE'];
 include_once "trafficFunctions.inc.php";
 include('phpgraphlib.php');
+$VISITS = showDayVisits($START_DATE, $START_HOUR, $END_DATE, $END_HOUR);
 $graph = new PHPGraphLib(500, 450);
-$data = array("Today" => showDayVisits($START_DATE, $START_HOUR, $END_DATE, $END_HOUR));//, "Feb" => -1200, "Mar" => -100, "Apr" => -1925,
+$data = array("Today" => $VISITS);//, "Feb" => -1200, "Mar" => -100, "Apr" => -1925,
 //		"May" => -1444, "Jun" => -957, "Jul" => -364, "Aug" => -221,
 //		"Sep" => -1300, "Oct" => -848, "Nov" => -719, "Dec" => -114);
 $graph->addData($data);
